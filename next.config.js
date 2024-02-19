3c4ce7426b98ea59f13918f2273c202fa8fedf4d
s.khomidov <khomidovsirojiddin@gmail.com>
2024-02-19 19:15:57 +0500

    next.config.js is changed

--- next.config.js
+++ next.config.js
@@ -1,24 +1,15 @@
 /** @type {import('next').NextConfig} */
 
-// const { locales, defaultLocale } = require("./i18n");
 const path = require("path");
 
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
     prependData: `
-        @import "./app/[lng]/styles/_mixins.scss";
-        @import "./app/[lng]/styles/unit.scss";
-        @import "./app/[lng]/styles/variables.scss";
-        `,
-    additionalData: `
-        @import "./app/[lng]/styles/mixins.scss";
-        @import "./app/[lng]/styles/unit.scss";
-        @import "./app/[lng]/styles/variables.scss";
-        `,
-  },
+        @import "./src/styles/mixins.scss";
+        @import "./src/styles/unit.scss";
+        `
+  }
 };
 
 module.exports = nextConfig;
