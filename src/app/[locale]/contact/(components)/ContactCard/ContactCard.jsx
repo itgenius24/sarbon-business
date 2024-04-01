c2316e46a2c8b8f15d8318420dcf157ebb0c9acf
mukhammadyn <nurmuhammadmahmudov421@gmail.com>
2024-04-01 15:56:07 +0500

    change: location

--- src/app/[locale]/contact/(components)/ContactCard/ContactCard.jsx
+++ src/app/[locale]/contact/(components)/ContactCard/ContactCard.jsx
@@ -13,6 +13,11 @@ export const ContactCard = ({
     <span className={clsx(cls.icon, cls[type])}></span>
     <h4 className={cls.title}>{title}</h4>
     <p className={cls.text}>{desc}</p>
-    <a className={cls.link} target="_blank" href={addressLink ? addressLink : type === "email" ? `mailto:${content}` : type === "phone" ? `tel:${content}` : content}>{content}</a>
+    {
+      type !== "location"
+        ? <a className={cls.link} target="_blank" href={addressLink ? addressLink : type === "email" ? `mailto:${content}` : type === "phone" ? `tel:${content}` : content}>{content}</a>
+        : <span className={cls.link}>{content}</span>
+    }
+
   </div>;
 };
