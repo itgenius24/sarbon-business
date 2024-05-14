89d9b4dd055c466bf3da0a9c248f4a707727a003
mukhammadyn <nurmuhammadmahmudov421@gmail.com>
2024-05-14 14:07:42 +0500

    change: rendering

--- src/app/[locale]/page.js
+++ src/app/[locale]/page.js
@@ -1,13 +1,6 @@
-"use client";
 import { Main } from "@/modules/Main";
-import { useTranslation } from "../i18n/client";
-import { useGetLang } from "@/hooks/useGetLang";
 
 export default function Home() {
 
-  const locale = useGetLang();
-
-  const { t } = useTranslation(locale, "translations");
-
-  return <Main t={t} />;
+  return <Main />;
 }
--- src/modules/Main/Main.jsx
+++ src/modules/Main/Main.jsx
@@ -1,12 +1,19 @@
+"use client";
 import clsx from "clsx";
 import { MobileApp } from "./components/MobileApp";
 import { News } from "./components/News";
 import styles from "./styles.module.scss";
 import { useMainProps } from "./useMainProps";
+import { useGetLang } from "@/hooks/useGetLang";
+import { useTranslation } from "@/app/i18n/client";
 
-export function Main({ t }) {
+export function Main() {
 
   const { banner } = useMainProps();
+
+  const locale = useGetLang();
+
+  const { t } = useTranslation(locale, "translations");
 
   return (
     <article className={clsx(styles.main, "fade-in")}>
