8ef883dbfb68c7eb5a7540677cfd5d2a929e1c31
boburnimatullayev <boburcoding@gmail.com>
2024-07-10 10:07:59 +0500

    bug fix

--- src/modules/GpsTracking/useGpsTrackingProps.js
+++ src/modules/GpsTracking/useGpsTrackingProps.js
@@ -291,7 +291,7 @@ export const useGpsTrackingProps = () => {
           lat,
           long,
           number: data.distance || "0",
-          car_type_id:watch("car_type") ? watch("car_type") : watch("load_type_id") ? watch("load_type_id") : '',
+          car_type_id:watch("car_type")?.value ? watch("car_type")?.value : watch("load_type_id")?.value ? watch("load_type_id")?.value : '',
           // car_type: {
           //   label:watch("car_type"),
           //   value:watch("load_type_id")
