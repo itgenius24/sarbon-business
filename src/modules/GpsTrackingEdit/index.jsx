ef44f81103b96d94fa22691f475a6264021df5c3
boburnimatullayev <boburcoding@gmail.com>
2024-08-31 08:58:04 +0500

    bug fix

--- src/modules/GpsTrackingEdit/index.jsx
+++ src/modules/GpsTrackingEdit/index.jsx
@@ -51,9 +51,13 @@ import {
 import React, { useCallback, useRef, useState } from "react";
 
 import cls from "./style.module.scss";
+import { TextFieldWithAddition } from "@/components/TextFieldWithAddition";
 import { Modal } from "@/components/Modal";
 import LoadingMap from "@/modules/Cargo/components/LoadingMap";
-
+import { Dropdown } from "@/components/Dropdown";
+import { CarList } from "@/modules/SearchCar/component/CarList/CarList";
+import { LoadingSpinner } from "@/components/LoadingSpinner";
+import { UseIcon } from "@/assets/icons/icons";
 import ReactDOMServer from "react-dom/server";
 import {
   Clusterer,
@@ -78,7 +82,7 @@ import DriverQuestion from "./components/DriverQuestion";
 import DriverGruz from "./components/DriverGruz";
 import DriverGruzGoods from "./components/DriverGruzGoods";
 import Cmap from "./components/Cmap";
-import {useGpsTrackingProps}  from "./useGpsTrackingProps";
+import { useGpsTrackingProps } from "./useGpsTrackingProps";
 
 /* eslint no-undef: 0 */ // --> OFF
 
@@ -388,132 +392,7 @@ export default function GpsTrackingModuleTets() {
           mapIcon={mapIcon}
           watch={watch}
         />
-        <Map
-          defaultState={{
-            center: coordinates,
-            zoom: 6,
-          }}
-          options={{
-            maxZoom: 17,
-            minZoom: 2,
-          }}
-          width="100%"
-          height={"100%"}
-          modules={["Placemark", "geocode", "control.SearchControl"]}
-        >
-          <TypeSelector
-            mapTypes={[
-              "yandex#map",
-              "yandex#satellite",
-              "yandex#hybrid",
-              "yandex#publicMap",
-            ]}
-          />
-          <SearchControl options={{ float: "right" }} />
-          <ZoomControl options={{ position: { bottom: "30vh", right: 4 } }} />
-
-          {getCarListProps?.data?.map((carInfo) => {
-            return (
-              <>
-                <Placemark
-                  key={carInfo?.id}
-                  geometry={[carInfo?.lat, carInfo?.long]}
-                  properties={{ balloonContent: balloonContent }}
-                  options={{
-                    iconLayout: "default#image",
-                    iconImageHref:
-                      "data:image/svg+xml;charset=UTF-8," +
-                      encodeURIComponent(
-                        mapIcon[carInfo?.users_id_data?.provisions?.[0]] ||
-                          GreenMapIcon
-                      ),
-                    iconImageSize:
-                      watch("users_id")?.value || watch("users_id2")?.value
-                        ? [45, 105]
-                        : [40, 52],
-                    iconImageOffset: [-15, -42],
-                  }}
-                  modules={["geoObject.addon.balloon"]}
-                  onBalloonOpen={(e) => {
-                    const placemark = e.get("target");
-                    const balloonInstance = placemark.balloon;
-                    balloonInstance.events.add("click", () => {
-                      cargoRef.current = carInfo;
-                      // setContendSingle(carInfo);
-                      if (
-                        carInfo?.users_id_data?.provisions?.[0] === "our_cargo"
-                      ) {
-                        setModalType("driverCheck");
-                      } else if (
-                        carInfo?.users_id_data?.provisions?.[0] ===
-                        "someone_cargo"
-                      ) {
-                        setModalType("driverQuestion");
-                      } else if (
-                        carInfo?.users_id_data?.provisions?.[0] ===
-                        "waiting_for_driver"
-                      ) {
-                        setModalType("driverExpectation");
-                      } else {
-                        setModalType("driverQuestion");
-                      }
-                    });
-                  }}
-                  onMouseEnter={(e) => handleMouseEnter(e, carInfo)}
-                />
-              </>
-            );
-          })}
-          {/* </Clusterer> */}
-
-          {locationData &&
-            locationData.map((item) => (
-              <Placemark
-                key={item?.id}
-                geometry={[
-                  item.location_name.split(",")[0] * 1,
-                  item.location_name.split(",")[1] * 1,
-                ]}
-                properties={{
-                  balloonContent: balloonContentCargo,
-                }}
-                options={{
-                  iconLayout: "default#image",
-
-                  iconImageHref:
-                    "data:image/svg+xml;charset=UTF-8," +
-                    encodeURIComponent(
-                      item?.order_status[0] === "occupied_cargo"
-                        ? MapLoadIcon
-                        : MapLoadGreenIcon
-                    ),
-                  iconImageSize: [40, 52],
-                  iconImageOffset: [-15, -42],
-                }}
-                onBalloonOpen={(e) => {
-                  const placemark = e.get("target");
-                  const balloonInstance = placemark.balloon;
-                  balloonInstance.events.add("click", () => {
-                    setLoadState(item);
-                    if (item?.order_status[0] === "occupied_cargo") {
-                      setModalType("driverGruzGoods");
-                    } else {
-                      setModalType("driverGruz");
-                    }
-                  });
-                }}
-                onMouseEnter={(e) => handleMouseEnterCargo(e, item)}
-
-                // onClick={(e) => {
-
-                //   // handlePlacemarkClick(e.get("target").getMap(), [
-                //   //   item.location_name.split(",")[0] * 1,
-                //   //   item.location_name.split(",")[1] * 1,
-                //   // ]);
-                // }}
-              />
-            ))}
-        </Map>
+    
         <div className={cls.modalWrap}>
           <Flex>
             <Box width={"100%"}>
