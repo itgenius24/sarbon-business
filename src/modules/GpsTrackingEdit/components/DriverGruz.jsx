4dd5f5e42a1cc7f8ae9f0ede796171b0682e3428
frontcoding <boburcoding@gmail.com>
2024-11-07 11:19:38 +0500

    ui ux bug fix

--- src/modules/GpsTrackingEdit/components/DriverGruz.jsx
+++ src/modules/GpsTrackingEdit/components/DriverGruz.jsx
@@ -19,13 +19,26 @@ import {
 import { Popup } from "@/components/Popup";
 import { useUpdateCargo } from "@/services/api";
 import authStore from "@/store/auth.store";
-import { Avatar, Box, Button, Flex, IconButton, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay } from "@chakra-ui/react";
+import {
+  Avatar,
+  Box,
+  Button,
+  Flex,
+  IconButton,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+} from "@chakra-ui/react";
 import { format } from "date-fns";
 import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
 
-const DriverGruz = ({ cls,loadState,setModalType,setOffset }) => {
-  console.log("loadState",loadState);
+const DriverGruz = ({ cls, loadState, setModalType, setOffset }) => {
+  console.log("loadState", loadState);
   const { t } = useTranslation();
 
   const [isPopupOpen, setPopupOpen] = useState(false);
@@ -34,36 +47,39 @@ const DriverGruz = ({ cls,loadState,setModalType,setOffset }) => {
   }
 
   const { mutate } = useUpdateCargo({
-    onSuccess:(res) => {
+    onSuccess: (res) => {
       setPopupOpen(false);
-      setModalType("filter")
+      setModalType("filter");
       // setOffset(0)
-      
     },
   });
- 
 
   const updateCar = () => {
     mutate({
-      data:{
-        guid: loadState?.guid,//yukni guidisi
+      data: {
+        guid: loadState?.guid, //yukni guidisi
         new_status: ["occupied_cargo"],
-        users_id_3: authStore.userData.id
-      }
+        users_id_3: authStore.userData.id,
+      },
     });
   };
   return (
     <div className={cls.filter}>
       <Flex flexDirection={"column"} rowGap={"10px"} alignItems={"flex-start"}>
-        <Flex justifyContent={"space-between" } width={"100%"} alignItems={"center"}>
+        <Flex
+          justifyContent={"space-between"}
+          width={"100%"}
+          alignItems={"center"}
+        >
           <Flex gap={3}>
             <Avatar name="Bobur Nimatllayey" src="#" />
             <Box>
               <p className={cls.version}>
                 <span>Груз добавил: </span>
               </p>
-              <p className={cls.userName}>{loadState?.users_id_data?.full_name}</p>
-
+              <p className={cls.userName}>
+                {loadState?.users_id_data?.full_name}
+              </p>
             </Box>
           </Flex>
           <IconButton
@@ -74,23 +90,31 @@ const DriverGruz = ({ cls,loadState,setModalType,setOffset }) => {
           />
         </Flex>
 
-
         <Box mt={3} className={cls.cardWrap}>
           <Flex gap={2}>
-          <div className={cls.startAIconWrapGreen}><div  className={cls.startAGreenIcon}>A</div> <div className={cls.line}></div> </div>
+            <div className={cls.startAIconWrapGreen}>
+              <div className={cls.startAGreenIcon}>A</div>{" "}
+              <div className={cls.line}></div>{" "}
+            </div>
             <Box>
-              <p className={cls.cardStartTitle}>{loadState?.city_id_data?.name}</p>
+              <p className={cls.cardStartTitle}>{loadState?.from} </p>
               <p className={cls.cardStartSubTitle}>
-                {loadState?.city_id_data?.address_id_data?.name} / <span>{format(loadState?.load_time,"yyyy-mm-dd")}</span>
+                {loadState?.from} /{" "}
+                <span>{format(loadState?.load_time, "yyyy-MM-dd")}</span>
               </p>
             </Box>
           </Flex>
           <Flex mt={5} gap={2}>
+            <div className={cls.startAIconWrapGreen}>
             <div className={cls.startBGreenIcon}>B</div>
+            </div>
             <Box>
-              <p className={cls.cardStartTitle}> {loadState?.city_id_2_data?.name}</p>
+              <p className={cls.cardStartTitle}>
+                {loadState?.to}
+              </p>
               <p className={cls.cardStartSubTitle}>
-                {loadState?.city_id_2_data?.address_id_data?.name} /<span> {format(loadState?.date,"yyyy-mm-dd")}</span>
+                {loadState?.to} /
+                <span> {format(loadState?.date, "yyyy-MM-dd")}</span>
               </p>
             </Box>
           </Flex>
@@ -107,7 +131,7 @@ const DriverGruz = ({ cls,loadState,setModalType,setOffset }) => {
                       <StoneIcon /> {loadState?.weight} т.
                     </Flex>
                     <Flex gap={1} alignItems={"center"}>
-                      <LoadOulineIcon />  {loadState?.volume_m3}m3
+                      <LoadOulineIcon /> {loadState?.volume_m3}m3
                     </Flex>
                   </Flex>
                 </Flex>
@@ -116,41 +140,75 @@ const DriverGruz = ({ cls,loadState,setModalType,setOffset }) => {
           </Flex>
           <Flex mt={3} justifyContent={"space-between"}>
             <p className={cls.cardStartSubTitle}>Cумма</p>
-            <p className={cls.cardStartSubTitle}>Тип оплаты: <span>{loadState?.map_id_data?.payment_type?.length > 15 ?  `${loadState?.map_id_data?.payment_type?.slice(0,15)}...`:loadState?.map_id_data?.payment_type }</span></p>
+            <p className={cls.cardStartSubTitle}>
+              Тип оплаты:{" "}
+              <span>
+                {loadState?.map_id_data?.payment_type?.length > 15
+                  ? `${loadState?.map_id_data?.payment_type?.slice(0, 15)}...`
+                  : loadState?.map_id_data?.payment_type}
+              </span>
+            </p>
           </Flex>
           <Flex mt={3} justifyContent={"space-between"} alignItems={"center"}>
-            <p className={cls.sumGreen}>{loadState?.bid_cash} {loadState?.currency_id_data?.code} </p>
-            <p className={cls.cardStartSubTitle}>Предоплата: <span> {loadState?.prepayment_percentage > 0 ?"Da" : "Нет" } </span></p>
+            <p className={cls.sumGreen}>
+              {loadState?.bid_cash} {loadState?.currency_id_data?.code}{" "}
+            </p>
+            <p className={cls.cardStartSubTitle}>
+              Предоплата:{" "}
+              <span>
+                {" "}
+                {loadState?.prepayment_percentage > 0 ? "Дa" : "Нет"}{" "}
+              </span>
+            </p>
           </Flex>
         </Box>
-        <Button onClick={() => setPopupOpen(true)} size={"lg"} className={cls.btngreen}>
-        Забронировать груз
+        <Button
+          onClick={() => setPopupOpen(true)}
+          size={"lg"}
+          className={cls.btngreen}
+        >
+          Забронировать груз
         </Button>
-
       </Flex>
       <Modal isOpen={isPopupOpen} isCentered>
         <ModalOverlay />
         <ModalContent>
           <ModalHeader>
-            <ModalGruzIcon  />
+            <ModalGruzIcon />
           </ModalHeader>
           <ModalCloseButton onClick={handleClosePopup} />
           <ModalBody>
-            <p style={{ fontWeight:600,fontSize:"18px" }}>Забронировать груз?</p>
-            <p style={{ fontWeight:500,fontSize:"14px" }}>Груз будет забронирован и недоступен для других диспетчеров.</p>
+            <p style={{ fontWeight: 600, fontSize: "18px" }}>
+              Забронировать груз?
+            </p>
+            <p style={{ fontWeight: 500, fontSize: "14px" }}>
+              Груз будет забронирован и недоступен для других диспетчеров.
+            </p>
           </ModalBody>
 
           <ModalFooter>
-            <Button style={{background:'white',border:'1px solid rgba(208, 213, 221, 1)',color:'black'}} onClick={handleClosePopup} className={cls.btnOutline} mr={3}>
-            Нет
+            <Button
+              style={{
+                background: "white",
+                border: "1px solid rgba(208, 213, 221, 1)",
+                color: "black",
+              }}
+              onClick={handleClosePopup}
+              className={cls.btnOutline}
+              mr={3}
+            >
+              Нет
             </Button>
-            <Button style={{background:'rgba(21, 186, 77, 1)'}} onClick={updateCar} className={cls.btngreen}>
-            Забронировать
+            <Button
+              style={{ background: "rgba(21, 186, 77, 1)" }}
+              onClick={updateCar}
+              className={cls.btngreen}
+            >
+              Забронировать
             </Button>
           </ModalFooter>
         </ModalContent>
       </Modal>
-    
     </div>
   );
 };
--- src/modules/GpsTrackingEdit/components/DriverGruzGoods.jsx
+++ src/modules/GpsTrackingEdit/components/DriverGruzGoods.jsx
@@ -21,12 +21,25 @@ import {
 } from "@/assets/icons/icons";
 import { useUpdateCargo } from "@/services/api";
 import authStore from "@/store/auth.store";
-import { Avatar, Box, Button, Flex, IconButton, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay } from "@chakra-ui/react";
+import {
+  Avatar,
+  Box,
+  Button,
+  Flex,
+  IconButton,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+} from "@chakra-ui/react";
 import { format } from "date-fns";
 import React, { useState } from "react";
 
-const DriverGruzGoods = ({ cls,setModalType,loadState }) => {
-  console.log("loadState",loadState);
+const DriverGruzGoods = ({ cls, setModalType, loadState }) => {
+  console.log("loadState", loadState);
 
   const [isPopupOpen, setPopupOpen] = useState(false);
   function handleClosePopup() {
@@ -34,63 +47,74 @@ const DriverGruzGoods = ({ cls,setModalType,loadState }) => {
   }
 
   const { mutate } = useUpdateCargo({
-    onSuccess:(res) => {
+    onSuccess: (res) => {
       setPopupOpen(false);
-      setModalType("filter")
+      setModalType("filter");
     },
   });
 
   const updateCar = () => {
     mutate({
-      data:{
-        guid: loadState?.guid,//yukni guidisi
+      data: {
+        guid: loadState?.guid, //yukni guidisi
         order_status: ["free_cargo"],
-        users_id_3: ""
-      }
+        users_id_3: "",
+      },
     });
   };
   return (
     <div className={cls.filter}>
       <Flex flexDirection={"column"} rowGap={"10px"} alignItems={"flex-start"}>
-        <Flex justifyContent={"space-between" } width={"100%"} alignItems={"center"}>
+        <Flex
+          justifyContent={"space-between"}
+          width={"100%"}
+          alignItems={"center"}
+        >
           <Flex gap={3}>
             <Avatar name="Bobur Nimatllayey" src="#" />
             <Box>
               <p className={cls.version}>
                 <span>Груз добавил: </span>
               </p>
-              <p className={cls.userName}>{loadState?.users_id_data?.full_name}</p>
-
+              <p className={cls.userName}>
+                {loadState?.users_id_data?.full_name}
+              </p>
             </Box>
           </Flex>
           <IconButton
             width={"fit-content"}
             style={{ background: "transparent" }}
-            icon={<CloseIconM /> }
+            icon={<CloseIconM />}
             onClick={() => setModalType(`filter`)}
-
           />
         </Flex>
-
 
         <Box mt={3} className={cls.cardWrap}>
           <Flex gap={2}>
             {/* <div className={cls.startAGoodsIcon}>A</div> */}
-            <div className={cls.startAIconWrapGoods}><div className={cls.startAGoodsIcon}>A</div> <div className={cls.line}></div> </div>
+            <div className={cls.startAIconWrapGoods}>
+              <div className={cls.startAGoodsIcon}>A</div>{" "}
+              <div className={cls.line}></div>{" "}
+            </div>
 
             <Box>
-              <p className={cls.cardStartTitle}>{loadState?.city_id_data?.name}</p>
+              <p className={cls.cardStartTitle}>{loadState?.from}</p>
               <p className={cls.cardStartSubTitle}>
-                {loadState?.city_id_data?.address_id_data?.name} / <span>{format(loadState?.load_time,"yyyy-mm-dd")}</span>
+                {loadState?.from} /{" "}
+                <span>{format(loadState?.load_time, "yyyy-MM-dd")}</span>
               </p>
             </Box>
           </Flex>
           <Flex mt={5} gap={2}>
-            <div className={cls.startBGoodsIcon}>B</div>
+          <div className={cls.startAIconWrapGoods}>
+          <div className={cls.startBGoodsIcon}>B</div>
+
+          </div>
             <Box>
-              <p className={cls.cardStartTitle}> {loadState?.city_id_2_data?.name}</p>
+              <p className={cls.cardStartTitle}> {loadState?.to}</p>
               <p className={cls.cardStartSubTitle}>
-                {loadState?.city_id_2_data?.address_id_data?.name} /<span> {format(loadState?.date,"yyyy-mm-dd")}</span>
+                {loadState?.to} /
+                <span> {format(loadState?.date, "yyyy-MM-dd")}</span>
               </p>
             </Box>
           </Flex>
@@ -117,45 +141,62 @@ const DriverGruzGoods = ({ cls,setModalType,loadState }) => {
           </Flex>
           <Flex mt={3} justifyContent={"space-between"}>
             <p className={cls.cardStartSubTitle}>Cумма</p>
-            <p className={cls.cardStartSubTitle}>Тип оплаты: <span>{loadState?.map_id_data?.payment_type?.length > 15 ? `${loadState?.map_id_data?.payment_type?.slice(0,15)}...`:loadState?.map_id_data?.payment_type }</span></p>
+            <p className={cls.cardStartSubTitle}>
+              Тип оплаты:{" "}
+              <span>
+                {loadState?.map_id_data?.payment_type?.length > 15
+                  ? `${loadState?.map_id_data?.payment_type?.slice(0, 15)}...`
+                  : loadState?.map_id_data?.payment_type}
+              </span>
+            </p>
           </Flex>
           <Flex mt={3} justifyContent={"space-between"} alignItems={"center"}>
-            <p className={cls.sum2}>{loadState?.bid_cash} {loadState?.currency_id_data?.code}</p>
-            <p className={cls.cardStartSubTitle}>Предоплата: <span>{loadState?.prepayment_percentage > 0 ?"Da" : "Нет" }</span></p>
+            <p className={cls.sum2}>
+              {loadState?.bid_cash} {loadState?.currency_id_data?.code}
+            </p>
+            <p className={cls.cardStartSubTitle}>
+              Предоплата:{" "}
+              <span>{loadState?.prepayment_percentage > 0 ? "Дa" : "Нет"}</span>
+            </p>
           </Flex>
         </Box>
-        {
-          loadState?.users_id_3_data?.guid === authStore.userData.id ? <Box className={cls.cardWrap}>
+        {loadState?.users_id_3_data?.guid === authStore.userData.id ? (
+          <Box className={cls.cardWrap}>
             <Flex gap={2} mb={3}>
               <CeckGoodsIcon />
               <Box>
                 <p className={cls.armorTitle}>Этот груз забронирован за вами</p>
                 <p className={cls.armorDate}>
-          До снятия брони: <span>
-          12:36:59
-                  </span>
+                  До снятия брони: <span>12:36:59</span>
                 </p>
               </Box>
             </Flex>
-            <Button onClick={() => setPopupOpen(true)} size={"lg"} className={cls.btngoods}>
-          Отменить бронь
+            <Button
+              onClick={() => setPopupOpen(true)}
+              size={"lg"}
+              className={cls.btngoods}
+            >
+              Отменить бронь
             </Button>
-          </Box>:<Box className={cls.cardWrap}>
-            <Flex width={'100%'} alignItems={'center'} gap={3}>
-              <Avatar name={loadState?.users_id_3_data?.full_name} src={loadState?.users_id_3_data?.photo} />
+          </Box>
+        ) : (
+          <Box className={cls.cardWrap}>
+            <Flex width={"100%"} alignItems={"center"} gap={3}>
+              <Avatar
+                name={loadState?.users_id_3_data?.full_name}
+                src={loadState?.users_id_3_data?.photo}
+              />
               <Box>
                 <p className={cls.cardStartSubTitle}>Диспетчер: </p>
                 <p className={cls.name}>
-                  {loadState?.users_id_3_data?.full_name} {loadState?.users_id_3_data?.your_id}
+                  {loadState?.users_id_3_data?.full_name}{" "}
+                  {loadState?.users_id_3_data?.your_id}
                 </p>
                 <p className={cls.cardStartSubTitle}>07.08.2024 / 12:36 </p>
-
               </Box>
             </Flex>
           </Box>
-        }
-
-
+        )}
       </Flex>
       <Modal isOpen={isPopupOpen} isCentered>
         <ModalOverlay />
@@ -165,16 +206,33 @@ const DriverGruzGoods = ({ cls,setModalType,loadState }) => {
           </ModalHeader>
           <ModalCloseButton onClick={handleClosePopup} />
           <ModalBody>
-            <p style={{ fontWeight:600,fontSize:"18px" }}>Забронировать груз?</p>
-            <p style={{ fontWeight:500,fontSize:"14px" }}>Груз будет забронирован и недоступен для других диспетчеров.</p>
+            <p style={{ fontWeight: 600, fontSize: "18px" }}>
+              Забронировать груз?
+            </p>
+            <p style={{ fontWeight: 500, fontSize: "14px" }}>
+              Груз будет забронирован и недоступен для других диспетчеров.
+            </p>
           </ModalBody>
 
           <ModalFooter>
-            <Button style={{ background:'white',border:'1px solid rgba(208, 213, 221, 1)',color:'black' }} onClick={handleClosePopup} colorScheme="blue" mr={3} >
-            Нет
+            <Button
+              style={{
+                background: "white",
+                border: "1px solid rgba(208, 213, 221, 1)",
+                color: "black",
+              }}
+              onClick={handleClosePopup}
+              colorScheme="blue"
+              mr={3}
+            >
+              Нет
             </Button>
-            <Button style={{ background:'rgba(193, 187, 32, 1)' }} onClick={updateCar} className={cls.btngreen}>
-            Отменить бронь
+            <Button
+              style={{ background: "rgba(193, 187, 32, 1)" }}
+              onClick={updateCar}
+              className={cls.btngreen}
+            >
+              Отменить бронь
             </Button>
           </ModalFooter>
         </ModalContent>
