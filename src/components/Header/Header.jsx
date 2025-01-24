39eab3e6991e4e0d7b2def578a3289f87b0d7f39
frontcoding <boburcoding@gmail.com>
2025-01-24 11:34:15 +0500

    bug fix locale

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -37,7 +37,7 @@ const Header = observer(({ elements }) => {
 
 
   const goToProfile = () => {
-    router.push(`/${locale}/profile`);
+    router.push(`/${locale ? locale : `ru`}/profile`);
   };
 
   const userData = useGetUserInfoHook();
