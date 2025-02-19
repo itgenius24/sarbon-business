2fcc0eb392060a65e3970bd42ad027d506c4db73
frontcoding <boburcoding@gmail.com>
2025-02-19 18:41:19 +0500

    bu fix

--- src/middleware.js
+++ src/middleware.js
@@ -7,28 +7,21 @@ export function middleware(request) {
   const cookies = request?.cookies 
   const company_id =  cookies.get("userData")  ?  JSON.parse(cookies.get("userData")?.value||{})?.role_id : null; // company_id
 
-  console.log(`company_id`,company_id)
 
-  // Ruxsat berilgan sahifalar bo'yicha ro'yxatlar
-  const roleOnePages = ["/ru/add-cargo", "/ru/my-load", "/ru/search-car"]; // role_id = f81d3c3d-228d-479e-a2b1-9948c98640f2 uchun sahifalar
-  const roleagesZ = ["/ru/gps-tracking",]; // role_id = 9bb1227a-0c90-4c70-bcee-b2563d32f7a0 uchun sahifalar
+
+
+  const roleOnePages = ["/ru/add-cargo", "/ru/my-load", "/ru/search-car","/dashboard"]; 
+  const roleagesZ = ["/dashboard"]; 
 
   const currentPath = request.nextUrl.pathname;
-
+  console.log(`company_id`, company_id,currentPath,request.url);
 
 
   if (roleOnePages.includes(currentPath) && company_id === `f81d3c3d-228d-479e-a2b1-9948c98640f2` ) {
-    return NextResponse.redirect(new URL("/ru/auth", request.url));
+    return NextResponse.redirect(new URL("/ru", request.url));
+  } else if (roleagesZ.includes(currentPath) && company_id === "48871d27-7361-4f69-8fe4-b54daf270739" ) {
+    return NextResponse.redirect(new URL("/ru", request.url));
   }
-
-  if (roleagesZ.includes(currentPath) && company_id === `Zakazcik` ) {
-    return NextResponse.redirect(new URL("/ru/auth", request.url));
-  }
-
- 
-//   if (roleOnePages.includes(roleTwoPages) && company_id) {
-//     return NextResponse.redirect(new URL("/uz/auth", request.url));
-//   }
 
   return NextResponse.next();
 }
