4bc4bf086c8c235b75298cc11169e7265daf34e1
frontcoding <boburcoding@gmail.com>
2025-02-20 18:55:21 +0500

    test

--- src/middleware.js
+++ src/middleware.js
@@ -65,6 +65,7 @@ export function middleware(request) {
   return NextResponse.next();
 }
 
+
 export const config = {
   matcher: [
     "/((?!api|_next/static|_next/image|_next/svg|svg|favicon.ico|images|auth|login).*)", // Maxsus marshrutlarni chetlab o'tish
