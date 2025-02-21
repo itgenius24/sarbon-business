d569541d62c619b70c8ef8d4cb543c7a9acc497e
frontcoding <boburcoding@gmail.com>
2025-02-21 17:47:00 +0500

    bu fix notification

--- src/layouts/MainLayout/MainLayout.jsx
+++ src/layouts/MainLayout/MainLayout.jsx
@@ -25,28 +25,28 @@ export const MainLayout = ({ locale, children }) => {
 
   const { mutate } = useUpdateNoteData();
 
-  const { data, isFetching } = useGetNoteListFirst({
-    params: {
-      data: JSON.stringify({
-        users_id_2: authStore.userData?.guid,
-        views: false,
-        with_relations: true,
-      }),
-    },
-    querySettings: {
-      enabled: Boolean(
-        authStore.userData?.role_id ===
-          "785678f2-fae7-4a00-8766-99ea67d3784f" &&
-          !pathname.includes(`my-loads`)
-      ),
-      onSuccess: (res) => {
-        if (res.response?.length > 0) {
-          notificationFn(res);
-        }
-      },
-      refetchInterval: 10000,
-    },
-  });
+  // const { data, isFetching } = useGetNoteListFirst({
+  //   params: {
+  //     data: JSON.stringify({
+  //       users_id_2: authStore.userData?.guid,
+  //       views: false,
+  //       with_relations: true,
+  //     }),
+  //   },
+  //   querySettings: {
+  //     enabled: Boolean(
+  //       authStore.userData?.role_id ===
+  //         "785678f2-fae7-4a00-8766-99ea67d3784f" &&
+  //         !pathname.includes(`my-loads`)
+  //     ),
+  //     onSuccess: (res) => {
+  //       if (res.response?.length > 0) {
+  //         notificationFn(res);
+  //       }
+  //     },
+  //     refetchInterval: 10000,
+  //   },
+  // });
 
   const notificationFn = (res) => {
     mutate({
--- src/modules/MyLoadsMain/useMyLoadsMainProps.js
+++ src/modules/MyLoadsMain/useMyLoadsMainProps.js
@@ -130,30 +130,7 @@ export const useMyLoadsMainProps = (locale) => {
 
   const { mutate } = useUpdateNoteData();
 
-  const { data: notification,  } = useGetNoteList({
-    params: {
-      data: JSON.stringify({
-        users_id_2: authStore.userData?.guid,
-        views: false,
-        with_relations: true,
-      }),
-    },
-    querySettings: {
-      enabled: Boolean(
-        authStore.userData?.role_id ===
-          "785678f2-fae7-4a00-8766-99ea67d3784f" &&
-          pathname.includes(`my-loads`)
-      ),
-      onSuccess: (res) => {
-        if (res.response?.length > 0) {
-          console.log(`res`, res);
-          notificationFn(res);
-        }
-      },
-      refetchInterval: 10000,
-    },
-  });
-
+  
 
   // const { data: notification2 } = useGetNoteList({
   //   params: {
