304bfb3a54d23164c433c80137f295c7d5a4b86d
frontcoding <boburcoding@gmail.com>
2025-02-26 19:34:14 +0500

    bug fix

--- src/modules/Dashboard/useDashboard.jsx
+++ src/modules/Dashboard/useDashboard.jsx
@@ -1021,12 +1021,7 @@ export const useDashboard = (locale) => {
         const our_cargo = row?.drivers_count?.our_cargo || 0;
         return (
           <p style={{ textAlign: `center` }}>
-            {empty +
-              someone_cargo +
-              broke_down +
-              newDriver +
-              unknown +
-              our_cargo}
+            {empty + someone_cargo + broke_down + newDriver + unknown + our_cargo}
           </p>
         );
       },
@@ -1036,11 +1031,7 @@ export const useDashboard = (locale) => {
       dataIndex: "",
       width: 200,
       render: (_, row) => (
-        <p style={{ textAlign: `center` }}>
-          {row?.drivers_count?.empty + row?.drivers_count?.unknown ||
-            0 - row?.drivers_count?.someone_cargo ||
-            0}
-        </p>
+        <p style={{ textAlign: `center` }}>{row?.drivers_count?.empty + row?.drivers_count?.unknown || 0 }</p>
       ),
     },
     {
