35302eb84abdd41e546d607868f9fbd955134535
frontcoding <boburcoding@gmail.com>
2025-03-03 17:39:33 +0500

    bug fix

--- src/layouts/MainLayout/elements.jsx
+++ src/layouts/MainLayout/elements.jsx
@@ -80,10 +80,10 @@ export const useElements = (lang) => {
         path: `/${lang || "ru"}/`,
         label: "Главный",
       },
-      {
-        path: `/${lang || "ru"}/dashboard-dispatcher`,
-        label: "Аналитика",
-      },
+      // {
+      //   path: `/${lang || "ru"}/dashboard-dispatcher`,
+      //   label: "Аналитика",
+      // },
       {
         path: isAuth ? `/${lang || "ru"}/my-loads` : `/${lang || "ru"}/auth`,
         label: "Мои грузы",
