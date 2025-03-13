a4919d122fe8def2d0cac0f8de8c14cb19793528
frontcoding <boburcoding@gmail.com>
2025-03-13 22:20:17 +0500

    bug fix

--- src/app/[locale]/contact/(components)/ContactCard/ContactCard.jsx
+++ src/app/[locale]/contact/(components)/ContactCard/ContactCard.jsx
@@ -16,7 +16,7 @@ export const ContactCard = ({
     {
       type !== "location"
         ? <a className={cls.link} target="_blank" href={addressLink ? addressLink : type === "email" ? `mailto:${content}` : type === "phone" ? `tel:${content}` : content}>{content}</a>
-        : <a target="_blank" href={`https://yandex.com/maps/?ll=69.291684,41.340317&z=15&pt=69.291684,41.340317pm2rdm`} className={cls.link}>{content}</a>
+        : <a target="_blank" href={`https://yandex.uz/maps/10335/tashkent/?ll=69.292768%2C41.340453&mode=whatshere&pt=69.291684%2C41.340317pm2rdm&whatshere%5Bpoint%5D=69.292878%2C41.340541&whatshere%5Bzoom%5D=17&z=20.52`} className={cls.link}>{content}</a>
     }
   </div>;
 };
--- src/app/[locale]/contact/(components)/Locations/Locations.jsx
+++ src/app/[locale]/contact/(components)/Locations/Locations.jsx
@@ -9,7 +9,7 @@ export const Locations = ({ location, t, isLargerThan768 }) => {
       <Heading fontSize={isLargerThan768 ? "36px" : "24px"} lineHeight="44px" mb={isLargerThan768 ? "50px" : "16px"}>{t("Наши локaции")}</Heading>
       <Box display="flex" columnGap="16px" alignItems="center">
         <span className={cls.icon}></span>
-        <a target="_blank" href={`https://yandex.com/maps/?ll=69.291684,41.340317&z=15&pt=69.291684,41.340317pm2rdm`} className={cls.text}>{location}</a>
+        <a target="_blank" href={`https://yandex.uz/maps/10335/tashkent/?ll=69.292768%2C41.340453&mode=whatshere&pt=69.291684%2C41.340317pm2rdm&whatshere%5Bpoint%5D=69.292878%2C41.340541&whatshere%5Bzoom%5D=17&z=20.52`} className={cls.text}>{location}</a>
       </Box>
     </Container>
   </Box>;
