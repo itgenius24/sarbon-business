b1acd909a5af694ba390c076a6b852b433a1086a
frontcoding <boburcoding@gmail.com>
2025-03-18 21:20:02 +0500

    bug fix

--- src/layouts/MainLayout/elements.jsx
+++ src/layouts/MainLayout/elements.jsx
@@ -85,7 +85,7 @@ export const useElements = (lang) => {
         label: "Главный",
       },
       {
-        path: `/${lang || "ru"}/dashboard-dispatcher-top`,
+        path: `/${lang || "ru"}/dashboard-dispatcher`,
         label: "Dashboard",
       },
       {
@@ -112,7 +112,7 @@ export const useElements = (lang) => {
       // },
       {
         path: isAuth
-          ? `/${lang || "ru"}/gps-tracking-dispatcher-top`
+          ? `/${lang || "ru"}/gps-tracking-dispatcher`
           : `/${lang || "ru"}/auth`,
         label: "GPS tracking",
       },
