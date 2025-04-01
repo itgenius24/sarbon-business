fcdaca8af982201a742b5d9c1240ce44ff740753
frontcoding <boburcoding@gmail.com>
2025-04-01 10:46:34 +0500

    bug fix

--- src/modules/MyLoadsMain/components/AllPage/useProps.js
+++ src/modules/MyLoadsMain/components/AllPage/useProps.js
@@ -16,15 +16,13 @@ const useProps = (orderStatus, t, search, address) => {
   const role_id = authStore.userData.role_id;
   const userId = authStore.userData.id;
   const [limit, setLimit] = useState(0);
+
   const [data, setData] = useState([]);
-  const { mutate: actionCreate } = useCreateActionHistoriesMutation();
-
-
   const getAllUserCargo = useGetUserCargoAll({
     data: {
       data: {
         row_view_id: "5e8b88e5-5621-4761-8816-904e92ef1ec0",
-        offset: 0,
+        offset: limit,
         order: {},
         view_fields: [
           "package_quantity",
@@ -57,9 +55,8 @@ const useProps = (orderStatus, t, search, address) => {
           "country_from",
           "country_to",
         ],
-        search:
-         search.length > 0 ? search  :  ``,
-        limit: 1000,
+        search: search.length > 0 ? search : ``,
+        limit: search.length  > 0 ? 1000 : 100,
         users_id: ["b1ce9e78-273d-4591-af58-1912c8cba680"],
         cargo_type: ["cargo"],
       },
@@ -68,15 +65,27 @@ const useProps = (orderStatus, t, search, address) => {
       enabled: Boolean(orderStatus === `` || address),
       refetchOnWindowFocus: false,
       onSuccess: (res) => {
-        const resData = res?.response || [];
-        // if (search.length > 0) {
-          setData(resData || []);
-        // } else {
-          // setData([...data, ...resData]);
-        // }
+        {
+          const resData = res?.response || [];
+
+          if (search.length > 0) {
+            // Qidiruv bo'lsa, faqat yangi datani chiqaramiz
+            setData(resData || []);
+          } else if (!address) {
+            // Pagination: Agar offset 0 bo‘lsa (birinchi sahifa), data tozalanadi
+            setData((prevData) =>
+              limit === 0 ? resData : [...prevData, ...resData]
+            );
+          } else {
+            // Agar boshqa holatlar bo‘lsa, data almashtiriladi
+            setData(resData || []);
+          }
+        }
       },
     },
   });
+
+  const { mutate: actionCreate } = useCreateActionHistoriesMutation();
 
   const addPage = () => {
     setLimit((prev) => prev + 100);
