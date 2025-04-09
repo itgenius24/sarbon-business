a0650acd02b0c09228c61afcdfecf8476bb88130
frontcoding <boburcoding@gmail.com>
2025-04-09 11:50:55 +0500

    bug fix

--- src/components/UploadImgMobile/UploadImgMobile.jsx
+++ src/components/UploadImgMobile/UploadImgMobile.jsx
@@ -1,10 +1,20 @@
 import cls from "./styles.module.scss";
 import Image from "next/image";
-import Link from "next/link";
-import { Box, Button, Spinner, useMediaQuery } from "@chakra-ui/react";
-import { useGetLang } from "@/hooks/useGetLang";
-import { DeleteIcon, UploadCloudBlueIcon } from "@/assets/icons/icons";
+import {
+  Box,
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerHeader,
+  DrawerOverlay,
+  Spinner,
+  useDisclosure,
+  useMediaQuery,
+} from "@chakra-ui/react";
 import { fileUpload } from "@/services/fileUpload";
+import { CameraIcon, PicturesIcon } from "@/assets/icons/icons";
 
 export const UploadImgMobile = ({
   watch,
@@ -20,124 +30,197 @@ export const UploadImgMobile = ({
   isLoading = false,
   setFileUploadLoading = () => {},
   uploadAi = () => {},
-  inputProps = {}
+  inputProps = {},
 }) => {
-  const locale = useGetLang();
-  
+  const { onClose, onOpen, isOpen } = useDisclosure();
+
   const [isLargerThan845] = useMediaQuery("(min-width: 845px)");
-  const handleImageUpload = async (e,) => {
+  const handleImageUpload = async (e) => {
     setLoading(true);
-    const result = await fileUpload(e,setFileUploadLoading);
-    // setValue("image", result?.link);
+    onClose()
+    const result = await fileUpload(e, setFileUploadLoading);
     setValue(name, process.env.NEXT_PUBLIC_MEDIA_URL + result?.link);
     uploadAi(process.env.NEXT_PUBLIC_MEDIA_URL + result?.link);
   };
 
   return (
-    <Box
-      className={cls.fields}
-      // display="flex"
-      columnGap="24px"
-      maxW="540px"
-      width="100%"
-    >
-      {watch(name)?.length > 0 ? (
-        <Box>
+    <>
+      <Box
+        className={cls.fields}
+        // display="flex"
+        columnGap="24px"
+        maxW="540px"
+        width="100%"
+      >
+        {watch(name)?.length > 0 ? (
+          <Box>
+            <Box
+              display="flex"
+              position="relative"
+              alignItems="center"
+              justifyContent="center"
+              ml="auto"
+              maxWidth={"540px"}
+              width="100%"
+              height={isLargerThan845 ? `165px` : "237px"}
+              borderRadius="12px"
+              border={`2px dashed  rgba(219, 216, 227, 1)`}
+              background={
+                isColor ? "rgba(246, 247, 248, 1)" : "rgba(255, 255, 255, 1)"
+              }
+              padding="16px 10px"
+            >
+              {isLoading ? (
+                <>
+                  <Spinner size="xl" />
+                </>
+              ) : (
+                <Image
+                  className={cls.img}
+                  src={watch(name)}
+                  alt="cargo"
+                  width={isLargerThan845 ? 250 : 300}
+                  height={isLargerThan845 ? 250 : 300}
+                  style={{
+                    width: `100%`,
+                    height: isLargerThan845 ? `150px` : `210px`,
+                    objectFit: `contain`,
+                    objectPosition: `center`,
+                  }}
+                />
+              )}
+            </Box>
+            {isLoading ? (
+              <>
+                <></>
+              </>
+            ) : (
+              <Box
+                width={`100%`}
+                height={`30px`}
+                borderRadius={`8px`}
+                zIndex={12323231111}
+                backgroundColor={`transparent`}
+                fontWeight={`400`}
+                fontSize={`16px`}
+                textAlign={`center`}
+                marginTop={`10px`}
+              >
+                <span
+                  onClick={() => {
+                    setValue(name, null);
+                  }}
+                  style={{
+                    paddingBottom: `5px`,
+                    borderBottom: `1px dashed red`,
+                    color: `red`,
+                    cursor: `pointer`,
+                  }}
+                >
+                  Удалить фото
+                </span>
+              </Box>
+            )}
+          </Box>
+        ) : (
           <Box
             display="flex"
-            position="relative"
             alignItems="center"
             justifyContent="center"
+            // mt="24px"
+            border={
+              errors?.[name]
+                ? `2px dashed red`
+                : `2px dashed  rgba(219, 216, 227, 1)`
+            }
+            borderRadius="12px"
+            background={
+              isColor ? "rgba(246, 247, 248, 1)" : "rgba(237, 239, 245, 1)"
+            }
+            as="label"
             ml="auto"
-            maxWidth={"540px"}
             width="100%"
             height={isLargerThan845 ? `165px` : "237px"}
-            borderRadius="12px"
-            border={`2px dashed  rgba(219, 216, 227, 1)`}
-            background={
-              isColor ? "rgba(246, 247, 248, 1)" : "rgba(255, 255, 255, 1)"
-            }
-            padding="16px 10px"
+            cursor={"pointer"}
+            onClick={onOpen}
           >
             {isLoading ? (
               <>
                 <Spinner size="xl" />
               </>
             ) : (
-              <Image
-                className={cls.img}
-                src={watch(name)}
-                alt="cargo"
-                width={isLargerThan845 ? 250 : 300}
-                height={isLargerThan845 ? 250 : 300}
-                style={{
-                  width: `100%`,
-                  height: isLargerThan845 ? `150px` : `210px`,
-                  objectFit:`contain`,
-                  objectPosition: `center`,
-                }}
-              />
+              <>
+                <Box
+                  display={"flex"}
+                  flexDirection={"column"}
+                  alignItems={"center"}
+                  width={"100%"}
+                >
+                  <Box
+                    mx="auto"
+                    width="100%"
+                    height="100%"
+                    display={"flex"}
+                    flexDirection={"column"}
+                    alignItems={"center"}
+                  >
+                    {icon}
+                  </Box>
+                  <Box
+                    fontSize={"14px"}
+                    fontWeight={400}
+                    textAlign="center"
+                    // p={"2px"}
+                    mt={"20px"}
+                    color={errors?.[name] ? `red` : "rgba(0, 122, 255, 1)"}
+                    borderBottom={`1.5px dashed ${
+                      errors?.[name] ? `red` : "rgba(0, 122, 255, 1)"
+                    }`}
+                    width={`fit-content`}
+                  >
+                    {text}
+                  </Box>
+                </Box>
+              </>
             )}
           </Box>
-          {isLoading ? (
-            <>
-              <></>
-            </>
-          ) : (
-            <Box
-              width={`100%`}
-              height={`30px`}
-              borderRadius={`8px`}
-              zIndex={12323231111}
-              backgroundColor={`transparent`}
-              fontWeight={`400`}
-              fontSize={`16px`}
-              textAlign={`center`}
-              marginTop={`10px`}
-            >
-              <span
-                onClick={() => {
-                  setValue(name, null);
-                }}
-                style={{
-                  paddingBottom: `5px`,
-                  borderBottom: `1px dashed red`,
-                  color: `red`,
-                  cursor: `pointer`,
-                }}
-              >
-                Удалить фото
-              </span>
-            </Box>
-          )}
-        </Box>
-      ) : (
-        <Box
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-          // mt="24px"
-          border={
-            errors?.[name]
-              ? `2px dashed red`
-              : `2px dashed  rgba(219, 216, 227, 1)`
-          }
-          borderRadius="12px"
-          background={
-            isColor ? "rgba(246, 247, 248, 1)" : "rgba(237, 239, 245, 1)"
-          }
-          as="label"
-          ml="auto"
-          width="100%"
-          height={isLargerThan845 ? `165px` : "237px"}
-          cursor={"pointer"}
+        )}
+      </Box>
+
+      <Drawer placement="bottom" onClose={onClose} isOpen={isOpen}>
+        <DrawerOverlay />
+        <DrawerContent
+          borderTopLeftRadius={`12px`}
+          borderTopRightRadius={`12px`}
         >
-          {isLoading ? (
-            <>
-              <Spinner size="xl" />
-            </>
-          ) : (
-            <>
+          <DrawerCloseButton />
+          <DrawerHeader></DrawerHeader>
+          <DrawerBody
+            borderTopLeftRadius={`12px`}
+            borderTopRightRadius={`12px`}
+            mb={`20px`}
+          >
+            <Box mb={`30px`} mt={`14px`}>
+              <p className={cls.drawerText}>Водител. удостоверение (права)</p>
+              <p className={cls.drawerSubText}>
+                Выберите, откуда загрузить документ
+              </p>
+            </Box>
+            <Box
+              padding={`14px`}
+              borderRadius={`8px`}
+              color={`white`}
+              as="label"
+              display={`flex`}
+              alignItems={`center`}
+              backgroundColor={`var(--primary-text)`}
+              justifyContent={`center`}
+              gap="8px"
+              fontSize={`18px`}
+              fontWeight={600}
+            >
+              <CameraIcon />
+              <span>Открыть камеру</span>
               <input
                 id={name}
                 name={name}
@@ -151,41 +234,39 @@ export const UploadImgMobile = ({
                 }}
                 {...inputProps}
               />
-              <Box
-                display={"flex"}
-                flexDirection={"column"}
-                alignItems={"center"}
-                width={"100%"}
-              >
-                <Box
-                  mx="auto"
-                  width="100%"
-                  height="100%"
-                  display={"flex"}
-                  flexDirection={"column"}
-                  alignItems={"center"}
-                >
-                  {icon}
-                </Box>
-                <Box
-                  fontSize={"14px"}
-                  fontWeight={400}
-                  textAlign="center"
-                  // p={"2px"}
-                  mt={"20px"}
-                  color={errors?.[name] ? `red` : "rgba(0, 122, 255, 1)"}
-                  borderBottom={`1.5px dashed ${
-                    errors?.[name] ? `red` : "rgba(0, 122, 255, 1)"
-                  }`}
-                  width={`fit-content`}
-                >
-                  {text}
-                </Box>
-              </Box>
-            </>
-          )}
-        </Box>
-      )}
-    </Box>
+            </Box>
+            <Box
+              padding={`14px`}
+              borderRadius={`8px`}
+              color={`var(--primary-text)`}
+              as="label"
+              display={`flex`}
+              alignItems={`center`}
+              border={`1px solid rgba(208, 213, 221, 1)`}
+              justifyContent={`center`}
+              gap="8px"
+              fontSize={`18px`}
+              fontWeight={600}
+              mt={`10px`}
+            >
+              <PicturesIcon />
+              <span>Выбрать из галереи</span>
+              <input
+                id={name}
+                name={name}
+                className="visually-hidden"
+                type="file"
+                accept="image/*"
+                {...register(name, rules)}
+                onChange={(e) => {
+                  handleImageUpload(e);
+                }}
+                {...inputProps}
+              />
+            </Box>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </>
   );
 };
