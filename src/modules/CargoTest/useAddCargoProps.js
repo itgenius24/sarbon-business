3c55fdaec3ef7c92d15d3393f45557fc2cb884bc
frontcoding <boburcoding@gmail.com>
2025-05-10 13:36:37 +0500

    fix

--- src/modules/CargoTest/useAddCargoProps.js
+++ src/modules/CargoTest/useAddCargoProps.js
@@ -105,12 +105,7 @@ export const useAddCargoProps = ({ id, status, locale, setCargoIndex }) => {
 
   const toast = useToast();
 
-  // useEffect(() => {
-  //   if (searchParams.get("isFirst") !== "true") {
-  //     router.push(pathname + "?isFirst=true");
-  //     router.refresh();
-  //   }
-  // }, []);
+
 
   function handleEditToggle() {
     setCanEdit(!canEdit);
