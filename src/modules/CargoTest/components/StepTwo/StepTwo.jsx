97741c6aa2064ea0c746827b980c6f751bb630aa
frontcoding <boburcoding@gmail.com>
2025-05-21 13:27:46 +0500

    refactor:yo date disabled

--- src/modules/CargoTest/components/StepTwo/StepTwo.jsx
+++ src/modules/CargoTest/components/StepTwo/StepTwo.jsx
@@ -218,6 +218,11 @@ const StepTwo = ({ status,locale }) => {
                         isDisabled={!canEdit || watch(`as_soon_as_a`)}
                         canEdit={canEdit}
                         onChange={(date) => {
+                           if(watch(`unloading`)[index]?.to_date){
+                            if(new Date(watch(`unloading`)[index]?.to_date) < date){
+                              setValue(`unloading[${index}].to_date`, date);
+                            }
+                          }
                           lodingChangeDate("loading", date, index, item?.guid);
                         }}
                         control={control}
