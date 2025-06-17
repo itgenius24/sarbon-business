22dc5855bc04506755ec6b6d453125acd416c870
frontcoding <boburcoding@gmail.com>
2025-06-17 11:44:21 +0500

    add: combo input disigin

---
+++ src/components/TextFieldWithAddition/useTextFieldWithAdditionPropsPayment.js
@@ -0,0 +1,42 @@
+import { useEffect, useRef, useState } from "react";
+import { useForm } from "react-hook-form";
+
+export const useTextFieldWithAdditionPropsPayment = () => {
+
+  const [isOpen, setOpen] = useState(false);
+
+  const { control: dropdownControl } = useForm();
+
+  const additionalDropdownRef = useRef(null);
+
+  function handleToggle () {
+    setOpen(!isOpen);
+  }
+
+  function handleClose () {
+    setOpen(false);
+  }
+
+  function onWindowClick (e) {
+    const dropdown = additionalDropdownRef.current;
+    if(!e.target.closest(`.${dropdown?.className}`)) {
+      handleClose();
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("click", onWindowClick);
+
+    return () => {
+      window.removeEventListener("click", onWindowClick);
+    };
+  });
+
+  return {
+    isOpen,
+    handleToggle,
+    handleClose,
+    dropdownControl,
+    additionalDropdownRef,
+  };
+};
