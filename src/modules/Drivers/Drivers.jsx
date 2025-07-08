badb8d91663758b17f00d45678e2ce4d41b20df1
frontcoding <boburcoding@gmail.com>
2025-07-08 20:11:55 +0500

    feat: driver  crate add UploadImgMobile

--- src/modules/Drivers/Drivers.jsx
+++ src/modules/Drivers/Drivers.jsx
@@ -40,8 +40,7 @@ import {
 import { useState } from "react";
 import FormInternationInput from "@/components/Input/FormInternationalInput";
 import { UploadImg } from "@/components/UploadImg";
-import { Checkbox } from "@/components/Checkbox";
-
+import { UploadImgMobile } from "@/components/UploadImgMobile";
 export const DriversModule = ({ locale }) => {
   const { t } = useTranslation(locale);
   const {
@@ -160,7 +159,11 @@ export const DriversModule = ({ locale }) => {
                   {check ? t(`Независимый водитель`) : t(`Зависимый водитель`)}
                 </p>
               </Flex>
-              <p className={cls.driver_dependency_description}>{ check ? t(`driver_dependency_description_on`) : t(`driver_dependency_description_off`)}</p>
+              <p className={cls.driver_dependency_description}>
+                {check
+                  ? t(`driver_dependency_description_on`)
+                  : t(`driver_dependency_description_off`)}
+              </p>
             </Box>
             {/* {!id && (
               <Box>
@@ -247,21 +250,39 @@ export const DriversModule = ({ locale }) => {
                 {t("Фото водительского удостоверения")}
               </p>
 
-              <UploadImg
-                isCrop
-                watch={watch}
-                setValue={setValue}
-                name={"drivers_license"}
-                icon={<Img3UploadIcon />}
-                text={t("Загрузить фото")}
-                errors={errors}
-                // register={register}
-                // rules={{ required: t("Загрузить фото") }}
-                type={`driver_pass`}
-                isLoading={loadingFront}
-                setLoading={setLoadingFront}
-                uploadAi={uploadAi}
-              />
+              {isLargerThan845 ? (
+                <UploadImg
+                  isCrop
+                  watch={watch}
+                  setValue={setValue}
+                  name={"drivers_license"}
+                  icon={<Img3UploadIcon />}
+                  text={t("Загрузить фото")}
+                  errors={errors}
+                  // register={register}
+                  // rules={{ required: t("Загрузить фото") }}
+                  type={`driver_pass`}
+                  isLoading={loadingFront}
+                  setLoading={setLoadingFront}
+                  uploadAi={uploadAi}
+                />
+              ) : (
+                <UploadImgMobile
+                  isCrop
+                  watch={watch}
+                  setValue={setValue}
+                  name={"drivers_license"}
+                  icon={<Img3UploadIcon />}
+                  text={t("Загрузить фото")}
+                  errors={errors}
+                  // register={register}
+                  // rules={{ required: t("Загрузить фото") }}
+                  type={`driver_pass`}
+                  isLoading={loadingFront}
+                  setLoading={setLoadingFront}
+                  uploadAi={uploadAi}
+                />
+              )}
             </Box>
           </Flex>
           <Flex
@@ -278,15 +299,28 @@ export const DriversModule = ({ locale }) => {
               <span className={cls.subTitle}>{t("(можно позже)")}</span>
             </p>
 
-            <UploadImg
-              isColor={true}
-              watch={watch}
-              setValue={setValue}
-              name={"photo"}
-              text={t("Загрузить фото")}
-              icon={<UserIcon2 />}
-              isCrop
-            />
+            {isLargerThan845 ? (
+              <UploadImg
+                isColor={true}
+                watch={watch}
+                setValue={setValue}
+                name={"photo"}
+                text={t("Загрузить фото")}
+                icon={<UserIcon2 />}
+                isCrop
+              />
+            ) : (
+              <UploadImgMobile
+                isColor={true}
+                watch={watch}
+                setValue={setValue}
+                name={"photo"}
+                text={t("Загрузить фото")}
+                icon={<UserIcon2 />}
+                isCrop
+              />
+            )}
+
             <p style={{ display: isLargerThan845 ? `block` : `none` }}>
               {t("Фото водителя")}
             </p>
